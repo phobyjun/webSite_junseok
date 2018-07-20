7f1be85bafd32ca8587a0f638bfc55305c62cb46
junseok <phobyjun@gmail.com>
2018-07-20 16:35:29 +0900

    Add php file to check id and password

--- apis/login_check.php
+++ apis/login_check.php
@@ -1,9 +1,39 @@
 <?php
 $id=$_POST['id'];
 $pw=$_POST['pw'];
-echo $id.$pw;
-if('junseok' == $id and '1234' == $pw)
-  echo"<script>alert('로그인 성공'); location.href='../Main_html.html';</script>";
-else
-  echo"<script>alert('비밀번호가 틀렸습니다. '); history.go(-1);</script>";
+
+$db = mysqli_connect('localhost', 'root', 'autoset', 'dasom');
+if(mysqli_connect_errno()){
+  echo "Failed to connect to MySQL: ".mysqli_connect_errno();
+}
+$pw_encode = md5($pw);
+$table_name = "user";
+$sql = "SELECT password FROM $table_name WHERE id='$id'";
+
+if($result = mysqli_query($db, $sql))
+{
+  if(mysqli_num_rows($result) == 0)
+  {
+      echo "<script>alert('No matched ID.');</script>";
+      echo "<script>window.location.replace('../login_html.html');</script>";
+  }
+  else
+  {
+      $row = mysqli_fetch_assoc($result);
+      if($row["password"] == $pw_encode) // 로그인 성공
+      {
+        // 리디렉션
+          echo "<script>alert('Login Succeed.');</script>";
+          echo "<script>location.href='../Main_html.html';</script>";
+      }
+      else
+      {
+          echo "<script>alert('Wrong Password.');</script>";
+          echo "<script>window.location.replace('../login_html.html');</script>";
+      }
+  }
+}
+
+mysqli_free_result($result);
+mysqli_close($db);
 ?>
